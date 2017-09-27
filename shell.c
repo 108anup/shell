1fce7d021b014acfc9571e18a9a4cd6f15319c8c
Anup Agarwal <108anup@gmail.com>
2017-09-27 22:13:07 +0530

    Untabify the file

--- shell.c
+++ shell.c
@@ -17,34 +17,34 @@ int main(int argc, char **argv){
   init();
 
   while (1){
-		int child_pid;
-		char *cmd_line;
+    int child_pid;
+    char *cmd_line;
 
     print_prompt();
 
     cmd_line = readline("");
     
-		cmd = parse_command(cmd_line);
+    cmd = parse_command(cmd_line);
 
     //TODO::
-		//record command in history list (GNU readline history ?)
+    //record command in history list (GNU readline history ?)
 
     //TODO:: execute :
     /*
     if ( isBuiltInCommand(cmd)){
       executeBuiltInCommand(cmd);
-    } else {		
+    } else {    
       childPid = fork();
       if (childPid == 0){
         executeCommand(cmd); //calls execvp  
-			
+      
       } else {
         if (isBackgroundJob(cmd)){
           record in list of background jobs
             } else {
           waitpid (childPid);
 
-        }		
+        }   
       }
     }*/
   }
