d72e42d38cb8b5de3276cd491baf5742843085f5
Anup Agarwal <108anup@gmail.com>
2017-10-02 11:34:04 +0530

    Update gitignore

--- deps/handler.d
+++
@@ -1,7 +0,0 @@
-build/handler.o: src/handler.c inc/handler.h inc/shell.h inc/parse.h
-
-inc/handler.h:
-
-inc/shell.h:
-
-inc/parse.h:
--- deps/shell.d
+++
@@ -1,7 +0,0 @@
-build/shell.o: src/shell.c inc/parse.h inc/shell.h inc/handler.h
-
-inc/parse.h:
-
-inc/shell.h:
-
-inc/handler.h:
